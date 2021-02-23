3150a2603025d17f988812c6f8019cc16353f2e7
haribhai <bisenharishankar@gmail.com>
2021-02-23 22:00:10 +0530

    Day1 : Added sign up sign in ui components

--- app/src/main/java/com/harianplanet/roaster/SignInActivity.java
+++ app/src/main/java/com/harianplanet/roaster/SignInActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-
-        // hide default Action Bar on top of the screen
-        getActionBar().hide();
+        setContentView(R.layout.activity_sign_in);
     }
 }
