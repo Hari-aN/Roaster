e48e5a647e4e960b43bebb7f838e886d214d38d0
haribhai <bisenharishankar@gmail.com>
2021-02-23 20:28:11 +0530

    Initial commit

---
+++ app/src/main/java/com/harianplanet/roaster/MainActivity.java
@@ -0,0 +1,17 @@
+package com.harianplanet.roaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //hide default app action bar on top
+        getActionBar().hide();
+    }
+}
